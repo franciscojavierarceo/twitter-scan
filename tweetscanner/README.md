26844073041a02f0129356f8008c27ba617806d7
franciscojavierarceo <arceofrancisco@gmail.com>
2022-06-29 20:48:33 -0600

    updated lock file and install

--- tweetscanner/README.md
+++ tweetscanner/README.md
@@ -11,3 +11,10 @@ If you have issues with your shell just run:
 poetry env use python3.7
 ```
 
+To properly start the app simply run:
+
+```
+poetry shell
+export $(xargs < .env)
+python manage.py runserver
+```
--- tweetscanner/tweetscanner/settings.py
+++ tweetscanner/tweetscanner/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)!)j3h+cyo2ghcgb3j=hnt!4-_^0a-t01zvz&%9#l5r2!h1x-w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
