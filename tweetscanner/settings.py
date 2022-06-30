1ffd584f6c599181da7bdeff3791800399b09549
franciscojavierarceo <arceofrancisco@gmail.com>
2022-06-29 22:18:29 -0600

    updated settings so it could build locally

--- tweetscanner/settings.py
+++ tweetscanner/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'tweetscanner.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
         default='postgresql://postgres:postgres@localhost:5432/tweetscanner',
         conn_max_age=600
     )
-
 }
 
+if DEBUG:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
