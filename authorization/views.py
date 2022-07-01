2dbf6a36112e086a4273ff038852622c58acd12c
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-01 14:52:44 -0600

    updated to log error

--- authorization/views.py
+++ authorization/views.py
@@ -11,28 +11,32 @@ from django.http import HttpResponseRedirect
 
 def twitter_login(request):
     print('calling twitter api')
-    twitter_api = TwitterAPI()
-    url, oauth_token, oauth_token_secret = twitter_api.twitter_login()
-    if url is None or url == "":
-        messages.add_message(
-            request, messages.ERROR, "Unable to login. Please try again."
-        )
-        return render(request, "authorization/error_page.html")
-    else:
-        twitter_auth_token = TwitterAuthToken.objects.filter(
-            oauth_token=oauth_token
-        ).first()
-        if twitter_auth_token is None:
-            twitter_auth_token = TwitterAuthToken(
-                oauth_token=oauth_token, oauth_token_secret=oauth_token_secret
+    try:
+        twitter_api = TwitterAPI()
+        url, oauth_token, oauth_token_secret = twitter_api.twitter_login()
+        if url is None or url == "":
+            messages.add_message(
+                request, messages.ERROR, "Unable to login. Please try again."
             )
-            twitter_auth_token.save()
+            return render(request, "authorization/error_page.html")
         else:
-            twitter_auth_token.oauth_token_secret = oauth_token_secret
-            twitter_auth_token.save()
-        
-        print(f'redirecting to {url}')
-        return redirect(url)
+            twitter_auth_token = TwitterAuthToken.objects.filter(
+                oauth_token=oauth_token
+            ).first()
+            if twitter_auth_token is None:
+                twitter_auth_token = TwitterAuthToken(
+                    oauth_token=oauth_token, oauth_token_secret=oauth_token_secret
+                )
+                twitter_auth_token.save()
+            else:
+                twitter_auth_token.oauth_token_secret = oauth_token_secret
+                twitter_auth_token.save()
+
+            print(f'redirecting to {url}')
+            return redirect(url)
+    except Exception as e:
+        print(f'failed to attempt login:\n{e}')
+        return render(request, 'authorization/index.html')
 
 
 def twitter_callback(request):
