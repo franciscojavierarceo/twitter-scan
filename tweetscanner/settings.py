12fa852088ea36c5ec936847ffe1d5f31190a9b9
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-03 10:50:23 -0600

    removing celery config references

--- tweetscanner/settings.py
+++ tweetscanner/settings.py
@@ -157,8 +157,8 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 # CELERY STUFF
 # BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
-CELERY_ACCEPT_CONTENT = ["application/json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = "US/Pacific"
+# CELERY_RESULT_BACKEND = "redis://localhost:6379"
+# CELERY_ACCEPT_CONTENT = ["application/json"]
+# CELERY_TASK_SERIALIZER = "json"
+# CELERY_RESULT_SERIALIZER = "json"
+# CELERY_TIMEZONE = "US/Pacific"
