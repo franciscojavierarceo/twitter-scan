1326513d0e81e086496530945aa01153192529fd
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-03 12:39:30 -0600

    including admin change

--- authorization/admin.py
+++ authorization/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import TwitterAuthToken, TwitterUser, TwitterUserSearched
+from .models import TwitterAuthToken, TwitterUser, TwitterUserSearched, Tweet
 
 
 admin.site.register(TwitterUserSearched)
 admin.site.register(TwitterAuthToken)
 admin.site.register(TwitterUser)
+admin.site.register(Tweet)
