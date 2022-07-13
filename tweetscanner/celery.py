e005c7c9dbadc1d212a73d86a78f4a3fac48e4b8
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-13 07:39:45 -0400

    removing twitter calls from celery

--- tweetscanner/celery.py
+++ tweetscanner/celery.py
@@ -9,18 +9,11 @@ app = Celery("tweetscanner", broker=os.getenv("CELERY_BROKER_URL"))
 django.setup()
 
 from django.conf import settings
-from authorization.utils import fetch_and_store_tweets
-
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task
-def twitter_scrape_task(screen_name: str):
-    print(fetch_and_store_tweets(screen_name=screen_name))
 
 @app.task(bind=True)
 def debug_task(self):
