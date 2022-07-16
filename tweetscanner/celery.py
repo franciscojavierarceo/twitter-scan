75cd80c9f8c18cab82a2f2d0099120fec5d23a20
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-16 16:02:25 -0600

    updated celery config

--- tweetscanner/celery.py
+++ tweetscanner/celery.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
 import os
 import django
-
 from celery import Celery
+from django.conf import settings
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweetscanner.settings")
 app = Celery("tweetscanner", broker=os.getenv("CELERY_BROKER_URL"))
-django.setup()
-
-from django.conf import settings
+#django.setup()
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
