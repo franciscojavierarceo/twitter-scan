0d1e7a3b3e2d7423deedd88b30a01edbd06ce321
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-17 08:45:52 -0600

    added logging

--- tweetscanner/settings.py
+++ tweetscanner/settings.py
@@ -159,10 +159,12 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 1500
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 # CELERY STUFF
-BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "US/Pacific"
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 1500
+
+print(f'loaded broker url @ {CELERY_BROKER_URL} and {CELERY_RESULT_BACKEND}')
