4e02f3bd7b5c87940239ca0995321f451ab5574d
franciscojavierarceo <arceofrancisco@gmail.com>
2022-07-17 09:05:36 -0600

    adding a celery ping

--- tweetscanner/celery.py
+++ tweetscanner/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import os
 import django
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
@@ -17,3 +18,15 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @app.task(bind=True)
 def debug_task(self):
     print("Request: {0!r}".format(self.request))
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello from celery'), name='add every 10')
+
+
+@app.task
+def test(arg):
+    print(arg)
+
